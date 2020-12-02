fc5967fe74bafddb66b7310d443f3361df3a3aff
Lenur <lenur10241992@mail.ru>
2020-12-02 12:11:26 +0300

    first commit

---
+++ js/1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = false;
+
+//console.log(qwerty);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Jonn",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, [] ];
+console.log(arr[1]);
