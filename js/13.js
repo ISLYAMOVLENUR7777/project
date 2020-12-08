0962938c881b7b3efe39305270b876015095f18f
Lenur <lenur10241992@mail.ru>
2020-12-08 19:40:49 +0300

    20_urok 08.12.2020

--- js/13.js
+++ js/13.js
@@ -6,17 +6,17 @@ if ( 4 == '4') {
     console.log('Error');
 }
 
-const num = 50;
+// const num = 50;
 
-if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Slishkom mnogo');
-}   else {
-        console.log('Ok!');
-}
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Slishkom mnogo');
+// }   else {
+//         console.log('Ok!');
+// }
 
-(num == 50) ? console.log('Ok!') : console.log('Error');
+// (num == 50) ? console.log('Ok!') : console.log('Error');
 
 const num = 50;
 
