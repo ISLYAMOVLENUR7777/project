eaaec310c7df3f5575fd0a6d06f8d639f7ee0909
Lenur <lenur10241992@mail.ru>
2020-12-13 19:36:11 +0300

    Learn JS book

---
+++ js/learnJavascript1.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* let admin, name;
+name = "Джон";
+admin = name;
+console.log(admin);
+
+let ourPlanetName = "Земля";
+console.log(ourPlanetName);
+
+let curentUserName = "Alfred";
+console.log(curentUserName); */
+
+/* let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ? */
+
+/* 
+let userName = prompt("Как Вас зовут?");
+
+alert(`Вас зовут ${userName}`);  */
+
+/* let value = true;
+console.log(typeof value);
+
+value = String(value);
+console.log(typeof value);
+
+alert('6' / '2'); */
+
+/* let delitel = '6',
+    delimoe = '2',
+    znac = delitel / delimoe;
+console.log(znac);
+console.log(typeof(delitel));
+console.log(typeof(delimoe));
+console.log(typeof(znac)); */
+
+
+/* let str = '1qwe';
+console.log(typeof(str));
+str = Number(str);
+console.log(typeof(str)); */
+
+/* let age = Number("qwe");
+console.log(age); */
+
+// console.log(Boolean(''));
+
+
+/* let x = 1;
+x = -x;
+
+console.log(x); */
+
+
+let x = 10,
+    y = 4,
+    z = x % y,
+    s = x ** y;
+console.log(z);
+console.log(s);
+
